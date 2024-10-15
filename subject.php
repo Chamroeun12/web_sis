797dc8740a95b941571edd41151eb0e9ac1ec31f
Chamroeun12 <114636551+Chamroeun12@users.noreply.github.com>
2024-10-15 12:35:47 +0700

    Update subject.php

--- subject.php
+++ subject.php
@@ -41,119 +41,127 @@ if (isset($_POST['btnsave'])) {
 ?>
 <?php include_once 'header.php'; ?>
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <!-- <?php print_r($data); ?> -->
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">|Subject</h1>
-                </div>
-                <!-- /.col -->
-                <div class="col-sm-6">
-                    <button type="button" class="btn btn-success float-sm-right" data-toggle="modal"
-                        data-target="#modal-lg">Create</button>
-                </div>
-                <!-- <div class="col-sm-6">
+    <div class="content">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <!-- <?php print_r($data); ?> -->
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">|Subject</h1>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-success float-sm-right" data-toggle="modal"
+                            data-target="#modal-lg">Create</button>
+                    </div>
+                    <!-- <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active">Dashboard v2</li>
                     </ol>
                 </div> -->
-                <!-- /.col -->
-            </div>
-            <div class="modal fade" id="modal-lg">
-                <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
+                    <!-- /.col -->
+                </div>
+                <div class="modal fade" id="modal-lg">
+                    <div class="modal-dialog modal-lg">
+                        <div class="modal-content">
 
-                        <div class="modal-header">
-                            <h4>|Create Subject</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <!-- Condition to Add or Edit student -->
+                            <div class="modal-header">
+                                <h4>|Create Subject</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <!-- Condition to Add or Edit student -->
 
-                        <!-- form add and edit student -->
-                        <form name="subjectForm" method="post" action="">
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <div class="row">
-                                        <div class="col-md-3">
-                                            <label for="inputName">Subject Name</label>
-                                            <input type="text" id="subName" name="sub_name" class="form-control"
-                                                value="">
-                                        </div>
-                                        <div class="col-md-3">
-                                            <div class="form-group">
-                                                <label for="inputStatus">Color</label>
-                                                <select id="" name="color" class="form-control custom-select">
-                                                    <option selected disabled>Select one</option>
-                                                    <option value="bg-primary">Primary</option>
-                                                    <option value="bg-secondary">Secondary</option>
-                                                    <option value="bg-success">Success</option>
-                                                    <option value="bg-danger">Danger</option>
-                                                    <option value="bg-warning">Warning</option>
-                                                    <option value="bg-info">Info</option>
-                                                </select>
+                            <!-- form add and edit student -->
+                            <form name="subjectForm" method="post" action="">
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <label for="inputName">Subject Name</label>
+                                                <input type="text" id="subName" name="sub_name" class="form-control"
+                                                    value="">
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-group">
+                                                    <label for="inputStatus">Color</label>
+                                                    <select id="" name="color" class="form-control custom-select">
+                                                        <option selected disabled>Select one</option>
+                                                        <option value="bg-primary">Primary</option>
+                                                        <option value="bg-secondary">Secondary</option>
+                                                        <option value="bg-success">Success</option>
+                                                        <option value="bg-danger">Danger</option>
+                                                        <option value="bg-warning">Warning</option>
+                                                        <option value="bg-info">Info</option>
+                                                    </select>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer justify-content-between">
-                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                                <input type="submit" value="Save" name="btnsave" class="btn btn-success">
-                                <!-- <button type="button" class="btn btn-primary">Save</button> -->
-                            </div>
-                        </form>
+                                <div class="modal-footer justify-content-between">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    <input type="submit" value="Save" name="btnsave" class="btn btn-success">
+                                    <!-- <button type="button" class="btn btn-primary">Save</button> -->
+                                </div>
+                            </form>
 
+                        </div>
+                        <!-- /.modal-content -->
                     </div>
-                    <!-- /.modal-content -->
+                    <!-- /.modal-dialog -->
                 </div>
-                <!-- /.modal-dialog -->
-            </div>
-            <!-- /.row -->
-            <!-- Foreach Loop -->
-            <div class="row mt-5">
-                <?php foreach ($data as $row) { ?>
-                <div class="col-lg-3 col-3">
-                    <div class="small-box <?php echo $row['Color']; ?>">
+                <!-- /.row -->
+                <!-- Foreach Loop -->
+                <div class="row mt-5">
+                    <?php foreach ($data as $row) { ?>
+                    <div class="col-lg-3 col-3">
+                        <div class="d-flex justify-content-center">
+                            <div class="small-box <?php echo $row['Color']; ?>">
+                                <div class="inner">
+                                    <h3 class="text-white"><?php echo $row['Subject_name']; ?></h3>
+                                    <p class="text-white">&nbsp;</p>
+                                    <div class="icon">
+                                        <i class="fas fa-graduation-cap"></i>
+                                    </div>
+                                </div>
 
-                        <div class="inner">
-                            <h3 class="text-white"><?php echo $row['Subject_name']; ?></h3>
-                            <p class="text-white">&nbsp;</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fas fa-graduation-cap"></i>
-                        </div>
-                        <div class="d-flex justify-content-between small-box-footer">
-                            <div>
-                                <!-- <a href="" class="text-white ml-3">
+                                <div class="d-flex justify-content-between small-box-footer">
+                                    <div>
+                                        <!-- <a href="" class="text-white ml-3">
                                     View &nbsp;
                                     <i class="fas fa-arrow-right fa-xs"></i>
                                 </a> -->
-                            </div>
-                            <div class="mr-3 dropup">
-                                <i class="nav-icon fas fa-ellipsis-h text-white" data-toggle="dropdown"></i>
-                                <div class="dropdown-menu">
-                                    <a href="update_subject.php?sub_id=<?= $row['SubID']; ?>" class="dropdown-item"><i
-                                            class="fa fa-edit text-success"></i> Edit</a>
-                                    <a href="all_condition.php?sub_id=<?php echo $row['SubID'] ?>"
-                                        onclick="return confirm('Do you want to delete this record?')"
-                                        class="dropdown-item"><i class="fa fa-trash text-danger"></i> Delete</a>
+                                    </div>
+
+                                    <div class="mr-3 dropup">
+                                        <i class="nav-icon fas fa-ellipsis-h text-white" data-toggle="dropdown"></i>
+                                        <div class="dropdown-menu">
+                                            <a href="update_subject.php?sub_id=<?= $row['SubID']; ?>"
+                                                class="dropdown-item"><i class="fa fa-edit text-success"></i>
+                                                Edit</a>
+                                            <a href="all_condition.php?sub_id=<?php echo $row['SubID'] ?>"
+                                                onclick="return confirm('Do you want to delete this record?')"
+                                                class="dropdown-item"><i class="fa fa-trash text-danger"></i>
+                                                Delete</a>
+                                        </div>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
 
                     </div>
+                    <?php } ?>
                 </div>
-                <?php } ?>
-            </div>
-        </div>
 
-    </div><!-- /.container-fluid -->
-</div>
+
+            </div><!-- /.container-fluid -->
+        </div>
+    </div>
 </div>
 
 
