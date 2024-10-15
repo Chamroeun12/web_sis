f523157d5914eb5c3ac43b8970788b651763463c
Chamroeun12 <114636551+Chamroeun12@users.noreply.github.com>
2024-10-15 10:36:07 +0700

    chamroeun

--- class.php
+++ class.php
@@ -66,7 +66,7 @@ $maxpage = ceil($temp['CountRecords'] / 10);
         <div class="col-12">
             <div class="card">
                 <form name="classform" method="post" action="">
-                    <div class="row">
+                    <div class="row mt-3 ml-2">
                         <div class="col-md-8">
                             <div class="form-group">
                                 <div class="input-group">
@@ -74,7 +74,7 @@ $maxpage = ceil($temp['CountRecords'] / 10);
                                     <select name="addclass" class="form-control">
                                         <option value="">--Select Class--</option>
                                         <?php foreach ($class as $row) : ?>
-                                            <option value="<?= $row['ClassID']; ?>"><?= $row['Class_name']; ?></option>
+                                        <option value="<?= $row['ClassID']; ?>"><?= $row['Class_name']; ?></option>
                                         <?php endforeach; ?>
                                     </select>
                                     <div class="ml-3">
@@ -87,44 +87,52 @@ $maxpage = ceil($temp['CountRecords'] / 10);
                         <div class="col-md-4">
                             <div class="form-group" style="width: 300px;">
                                 <input type="text" name="namesearch" class="search form-control float-right"
-                                       placeholder="Search" style="font-family:Khmer OS Siemreap;">
+                                    placeholder="Search" style="font-family:Khmer OS Siemreap;">
                             </div>
                         </div>
                     </div>
             </div>
 
-            <div class="card-body table-responsive p-0 text-sm">
-                <table class="table table-hover text-nowrap" style="font-family:Khmer OS Siemreap;" id="userTbl">
-                    <thead>
-                        <tr>
-                            <th>Check</th>
-                            <th>No</th>
-                            <th>Student Code</th>
-                            <th>English Name</th>
-                            <th>Khmer Name</th>
-                            <th>Gender</th>
-                        </tr>
-                    </thead>
-                    <tbody id="showdata">
-                        <?php if (isset($student)) { ?>
-                            <?php foreach ($student as $key => $value) { ?>
-                                <tr>
-                                    <td>
-                                        <div class="icheck-primary">
-                                            <input type="checkbox" name="addstu[]" value="<?php echo $value['ID']; ?>" id="check<?php echo $key; ?>">
-                                            <label for="check<?php echo $key; ?>"></label>
-                                        </div>
-                                    </td>
-                                    <td><?php echo ($key + 1); ?></td>
-                                    <td><?php echo $value['Stu_code']; ?></td>
-                                    <td><?php echo $value['En_name']; ?></td>
-                                    <td><?php echo $value['Kh_name']; ?></td>
-                                    <td><?php echo $value['Gender']; ?></td>
-                                </tr>
-                            <?php } ?>
-                        <?php } ?>
-                    </tbody>
-                </table>
+            <div class="card">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card-body table-responsive p-0 text-sm">
+                            <table class="table table-hover text-nowrap" style="font-family:Khmer OS Siemreap;"
+                                id="userTbl">
+                                <thead>
+                                    <tr>
+                                        <th>Check</th>
+                                        <th>No</th>
+                                        <th>Student Code</th>
+                                        <th>English Name</th>
+                                        <th>Khmer Name</th>
+                                        <th>Gender</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="showdata">
+                                    <?php if (isset($student)) { ?>
+                                    <?php foreach ($student as $key => $value) { ?>
+                                    <tr>
+                                        <td>
+                                            <div class="icheck-primary">
+                                                <input type="checkbox" name="addstu[]"
+                                                    value="<?php echo $value['ID']; ?>" id="check<?php echo $key; ?>">
+                                                <label for="check<?php echo $key; ?>"></label>
+                                            </div>
+                                        </td>
+                                        <td><?php echo ($key + 1); ?></td>
+                                        <td><?php echo $value['Stu_code']; ?></td>
+                                        <td><?php echo $value['En_name']; ?></td>
+                                        <td><?php echo $value['Kh_name']; ?></td>
+                                        <td><?php echo $value['Gender']; ?></td>
+                                    </tr>
+                                    <?php } ?>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
             </form>
             <!-- this form table -->
